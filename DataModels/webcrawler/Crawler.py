3552e51c85fa596bbe24327864bfa39cca17ea47
aliHitawala <ali.h.hita@gmail.com>
2015-09-17 19:02:35 -0500

    Added scaffolding for the project and a crawler file. Code is for POC for now.

---
+++ DataModels/webcrawler/Crawler.py
@@ -0,0 +1,28 @@
+__author__ = 'aliHitawala'
+import urlparse
+import mechanize
+
+
+url = "http://www.bikewale.com/used/bikes-in-bangalore/"
+sub_url = "http://www.bikewale.com/used/bikes-in-bangalore"
+br = mechanize.Browser()
+urls = [url]
+visited = [url]
+times = 100
+while len(urls)>0:
+    if times < 0:
+        break
+    try:
+        br.open(urls[0])
+        urls.pop(0)
+        for link in br.links():
+            newurl =  urlparse.urljoin(link.base_url,link.url)
+            if newurl not in visited and sub_url in newurl:
+                visited.append(newurl)
+                urls.append(newurl)
+                print newurl
+    except:
+        print "error"
+        urls.pop(0)
+
+print visited
